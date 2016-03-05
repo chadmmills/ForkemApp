5b2e96ea66d4ded4578adde340c673dec1e58eb6
Chad Mills <chad@whiteboard.is>
2016-03-05 09:40:22 -0600

    Hound use double quotes and shorten long lines.

--- spec/views/mealbooks/show.html.haml_spec.rb
+++ spec/views/mealbooks/show.html.haml_spec.rb
@@ -1,12 +1,15 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "mealbooks/show" do
   it "should render a meal for an assigned day" do
     mealbook = create :mealbook
     tuesday_meal = create :meal, mealbook: mealbook
-    tuesday_assignment = create :meal_assignment, meal: tuesday_meal, assigned_on: Date.today.monday + 1
+    create :meal_assignment, meal: tuesday_meal, assigned_on: Date.today.monday + 1
 
-    render template: "mealbooks/show", locals: { mealbook: MealbookPresenter.new(mealbook) }
+    render(
+      template: "mealbooks/show",
+      locals: { mealbook: MealbookPresenter.new(mealbook) }
+    )
 
     expect(rendered).to have_css "#tuesday .meal"
   end
