dbfc880dd06c9a672d753d3d5bd3b68418abf6f3
Chad Mills <chad@whiteboard.is>
2016-03-06 08:38:16 -0600

    Show removal x only within weekday column.

--- spec/views/mealbooks/show.html.haml_spec.rb
+++ spec/views/mealbooks/show.html.haml_spec.rb
@@ -17,4 +17,22 @@ RSpec.describe "mealbooks/show" do
 
     expect(rendered).to have_css "#tuesday .meal"
   end
+
+  it "should show the remove icon within a weekday but not unassigned list" do
+    mealbook = create :mealbook
+    unassigned_meal = create :meal, mealbook: mealbook
+    tuesday_meal = create :meal, mealbook: mealbook
+    create(
+      :meal_assignment,
+      meal: tuesday_meal,
+      assigned_on: Date.today.monday + 1
+    )
+
+    render(
+      template: "mealbooks/show",
+      locals: { mealbook: MealbookPresenter.new(mealbook) }
+    )
+
+    expect(rendered).to have_css "#tuesday .meal .meal-unassign"
+  end
 end
